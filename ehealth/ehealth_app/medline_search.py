604fed496d2c6312979671498d30010ec85dca45
Calum <2082101Y@student.gla.ac.uk>
2016-03-20 02:12:24 +0000

    medline kind of works. Enough for you to implement the readability thing.

--- ehealth/ehealth_app/medline_search.py
+++ ehealth/ehealth_app/medline_search.py
@@ -1,27 +1,29 @@
 import urllib2, urllib
+from xml.dom import minidom
 
 base_url= 'https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term='
+ret_type = '&rettype=topic'
 
-
+					
 def run_medline_query(search_terms):
 	term = "{0}".format(search_terms)
 	term = urllib.quote(term)
 	
 	results = []
 	
-	url= (base_url + term)
+	url= (base_url + term + ret_type)
 	
-	response = urllib2.urlopen(url).read()
+	dom = minidom.parse(urllib.urlopen(url)) 
 	
-	
-	
-	for result in response['nlmSearchResult']['document']:
+	for result in dom.getElementsByTagName('document'):
+			print result.getAttribute('rank')
 			results.append({
-			'title': result['content']['title'],
-			'link': result['url'],
-			'from': "From:     Medline",
-			'summary': result['content']['snippet']
+			'title': result.getAttribute('rank'),  ##placeholder until I can figure out how to access title
+			'link': result.getAttribute('url'),
+			'from': "From:     Medline Plus",
+			'summary': 'content'  ## placeholder until I can access summary
 			})
+			
 			
 		
 	return results
